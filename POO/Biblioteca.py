be7cbd5c6d6f41ab5f17c8e8233675bcc7b98e47
Pedro Abelardo <70430136+maxiusofmaximus@users.noreply.github.com>
2023-11-27 20:00:08 -0500

    Update Biblioteca.py

--- POO/Biblioteca.py
+++ POO/Biblioteca.py
@@ -25,6 +25,7 @@ class Libro:
         self.titulo = titulo
         self.autor = autor
         self.isbn = isbn
+        self.disponibilidad = True
 
 
 class Biblioteca:
@@ -35,6 +36,8 @@ class Biblioteca:
 
     def agregar_libro(self, libro):
         self.libros.append(libro)
+        sentencia = f"INSERT INTO libros (titulo, autor, isbn, disponibilidad) VALUES {(self.libros[0].titulo, self.libros[0].autor, self.libros[0].isbn, self.libros[0].disponibilidad)}"
+        self.base_de_datos.ejecutar_sentencia(sentencia)
 
     def mostrar_libros_disponibles(self):
         sentencia = "SELECT * FROM libros"
@@ -64,40 +67,41 @@ class Biblioteca:
 
 
 if __name__ == "__main__":
-    base_de_datos = BaseDeDatos("localhost", "root", "", "biblioteca")
+    base_de_datos = BaseDeDatos("localhost", "root", "1234", "biblioteca")
     biblioteca = Biblioteca(base_de_datos)
 
     while True:
-        select = int(input("Ingrese la opción que desea realizar\n" +
+        select = input("Ingrese la opción que desea realizar\n" +
                            "1. Agregar un libro a la biblioteca\n" +
                            "2. Eliminar un libro de la biblioteca\n" +
                            "3. Mostrar los libros de la biblioteca\n" +
                            "4. Prestar el libro\n" +
                            "5. Devolver el libro\n" +
                            "6. Salir del menú\n" +
-                           "$ "))
-
+                           "$ ")
+        
         match select:
-            case 1:
+            case "1":
                 libro = Libro(input("Ingrese el titulo del libro a agregar: "), input(
                     "Ingrese el autor del libro a agregar: "), input("Ingrese el ISBN del libro a agregar: "))
+                
                 biblioteca.agregar_libro(libro)
 
-            case 2:
+            case "2":
                 id_libro = input("Ingrese el id del libro a eliminar: ")
                 sentencia = f"DELETE FROM libros WHERE id = {id_libro}"
                 base_de_datos.ejecutar_sentencia(sentencia)
 
-            case 3:
+            case "3":
                 biblioteca.mostrar_libros_disponibles()
 
-            case 4:
+            case "4":
                 biblioteca.prestar_libros()
 
-            case 5:
+            case "5":
                 biblioteca.devolver_libros()
 
-            case 6:
+            case "6":
                 print("Has salido del menú de opciones")
                 break
             case _:
